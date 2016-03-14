e6dd407e8dbbb39f9d58f162cd1c1fcdf04da50b
Alexis Hancock <lxshancock@gmail.com>
2016-03-14 02:03:05 -0400

    Style changes and add link

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,20 +1,23 @@
-$(document).ready(function() {
+$(document).ready( function() {
+
 	 $.getJSON("https://api.myjson.com/bins/1drql", function (data) {
 
-	 	$(".choose-a-major").click(function(event) {
-	 		$(".majors-listed").empty();
-				var random_num   = Math.floor((Math.random() * 39) + 1);
+	 	$( ".choose-a-major" ).click( function( event ) {
+
+	 		$( ".majors-listed" ).empty();
+
+				var random_num   = Math.floor( ( Math.random() * 39 ) + 1 );
 				var degree_name  = data[random_num]['Degree Name'];
 				var degree_field = data[random_num]['Category'];
 				var degree_type  = data[random_num]['Degree Type'];
 				var degree_link  = data[random_num]['Link'];
 
-				var html = "<li id='" +  random_num + "'>Field: " + degree_field + "</li>";
-					html += "<li id='" +  random_num + "'>Name: " + degree_name + "</li>";
-					html += "<li id='" +  random_num + "'>Degree Type: " + degree_type + "</li>";
+				var html = "<li id='" +  "field-" + random_num + "'>Field: " + degree_field + "</li>";
+					html += "<li id='" +  "name-" + random_num + "'>Name: " + degree_name + "</li>";
+					html += "<li id='" +  "degree" + random_num + "'>Degree Type: " + degree_type + "</li>";
 					html += "<a target=" + '_blank' + " href=" + degree_link + " id=" +  random_num + ">Link: " + degree_link + "</a>";
 
-		 	$(".majors-listed").append(html);
+		 	$( ".majors-listed" ).append( html );
 		});
 	});
 });
